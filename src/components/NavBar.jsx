f2aa799504af22085f6ed8eb340a2356e326a03d
Ayush Mishra <100191388+DraGonoff69@users.noreply.github.com>
2023-10-06 17:36:41 +0530

    final

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 export default function NavBar(props) {
     return (
       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-        <a className="navbar-brand mx-3" href="/">TextUtils</a>
+        <a className="navbar-brand mx-3" href="/">TextWizard</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
